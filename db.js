b8afe55a38e3f435bf9ab1cefafa3c0d61880bf1
Sandeep <sandeepnadegouni7282@gmail.com>
2024-01-07 14:26:11 +0530

    updated

--- db.js
+++ db.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const URI = process.env.MONGO_CLOUD_URI;
-const connectToMongo = () => {
-    mongoose.connect(URI, {
+const connectToMongo = async () => {
+  await mongoose.connect(URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         serverSelectionTimeoutMS: 30000,
-        bufferCommands: false, // Disable command buffering
-        bufferTimeoutMS: 15000, // Set timeout to 30 seconds (default is 30 seconds)
       })
     .then(() => { 
         console.log("Database connected successfully");
