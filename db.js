1558454c6bad249f3498bd169d8316ea31a67baf
Sandeep <sandeepnadegouni7282@gmail.com>
2024-01-07 14:24:06 +0530

    updated

--- db.js
+++ db.js
@@ -6,7 +6,9 @@ const connectToMongo = () => {
     mongoose.connect(URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 30000, // Set timeout to 30 seconds (default is 30 seconds)
+        serverSelectionTimeoutMS: 30000,
+        bufferCommands: false, // Disable command buffering
+        bufferTimeoutMS: 15000, // Set timeout to 30 seconds (default is 30 seconds)
       })
     .then(() => { 
         console.log("Database connected successfully");
